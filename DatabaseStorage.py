da5a21437b795dac3bc5c1b07332585185fea893
Gasay <egor0poletaykin@gmail.com>
2022-06-20 22:43:20 +0300

    class Creator + func TypeFinder

--- DatabaseStorage.py
+++ DatabaseStorage.py
@@ -2,6 +2,11 @@ import psycopg2
 from prettytable import PrettyTable
 import random as rnd
 import os
+import platform
+from clear_screen import clear
+
+
+
 
 class DatabasesNames:
     def __init__(self, database, table_name):
@@ -38,7 +43,31 @@ class Selector(Commands):
                 column = len(coulums)
                 rtable.add_row(dt[:column])
                 dt = dt[column:]
+            clear()
             print(rtable)
+
+
+        def file_executor(file_name=''):
+            if file_name == '':
+                file_name = input("File name or path to the file: ")
+            if file_name.count("/") != 0:
+                file_name.replace("/", "\\\\")
+                print(file_name)
+            with open(file_name, 'r') as f:
+                text_of_query = []
+                for line in f:
+                    text_of_query.append(line.strip('\n'))
+            query =  ' '.join(text_of_query)
+            cur.execute(f'''{query}''')
+            query_output_logic(cur.fetchall(), cur.description)
+            decision = input("Do you want to run the query again? (Y/n) y - Default: ")
+            if decision.lower() == 'n' or decision.lower() == 'no':
+                main_menu()
+                print("Closed successfully")
+                exit(0)
+            file_executor(file_name)
+
+
         def decision_func(cur, table_name):
             '''Forces you to make a choice between two options
             '''
@@ -62,8 +91,12 @@ class Selector(Commands):
                 '''
                 print("This module in development mode")
                 def decision2_func():
+                    ''' choice between manualy type and execute ready query from a file
+                    '''
                     print("Do you want to type query manualy(1) or execute ready query from a file(2) ?")
                     decision2 = input("# ")
+                    clear()
+                    #
                     if decision2 == str(1):
                         print("Write EOF in the end of query or pres ENTER twice")
                         print("SQL ")
@@ -79,7 +112,20 @@ class Selector(Commands):
                                 text_of_query.append(user_query)
                                 if user_query == '':
                                     enter = 2
-                        
+                        decision4 = input("Do you want to change the query? (Y/n) n - default: ")
+                        if decision4.lower() == 'y' or decision4.lower() == 'yes':
+                            # generate a random name of file and return ready query in .txt
+                            file_name = 'query_'+str(rnd.randint(0, 100000))+'.txt'
+                            with open(file_name, 'w+') as f:
+                                for line in range(len(text_of_query)):
+                                    f.write(text_of_query[line]+'\n')
+                            os.system(f"{file_name}")
+                            wait_until_done = input("# Waiting for your changes...(ENTER) ")
+                            file_executor(file_name)
+                            main_menu()
+                            print("Closed successfully")
+                            exit(0)
+                        #
                         cur.execute(f'''{query}''')
                         query_output_logic(cur.fetchall(), cur.description)
                         save_decision = input("Save the query text? (Y/n): ")
@@ -93,26 +139,10 @@ class Selector(Commands):
                                     f.write(text_of_query[line]+'\n')
                             print(f"Saved in {file_name}")
                     elif decision2 == str(2):
-                        def file_executor(file_name=''):
-                            if file_name == '':
-                                file_name = input("File name or path to the file: ")
-                            if file_name.count("/") != 0:
-                                file_name.replace("/", "\\\\")
-                                print(file_name)
-                            with open(file_name, 'r') as f:
-                                    text_of_query = []
-                                    for line in f:
-                                        text_of_query.append(line.strip('\n'))
-                            query =  ' '.join(text_of_query)
-                            cur.execute(f'''{query}''')
-                            query_output_logic(cur.fetchall(), cur.description)
-                            decision = input("Do you want to run the query again? (Y/n) y - Default: ")
-                            if decision.lower() == 'n' or decision.lower() == 'no':
-                                main_menu()
-                            file_executor(file_name)
                         file_executor()
-                        
-                        
+                        main_menu()
+                        print("Closed successfully")
+                        exit(0)
                     else:
                         # TODO: сделать try except error рекурсии
                         print("Please select 1 or 2")
@@ -126,11 +156,56 @@ class Selector(Commands):
         decision_func(cur, table_name)
         
 
+#! остановился тут
+def TypeFinder(x):
+    try:
+        if x == True or x == False:
+            return 'bool'
+        int(x)
+        return 'int'
+    except ValueError:
+        try:
+            float(x)
+            return 'float'
+        except ValueError:
+            if x.lower().startswith('ENUM') :
+                return 'enum'
+            return 'str'
 
 # class Insertor(Command):
 #     def base_method(self):
 #         cur.execute(f'''INSERT INTO {DataOfDatabases.table} '''
 
+class Creator(Commands):
+    def base_method(self, commands, con, DataOfDatabases):
+        cur = super().base_method(commands, con)
+        DataOfDatabases.table = input("Table name: ")
+        temp_data_of_columns = ''
+        temp_data_of_columnsArray = []
+        #
+        count_of_columns = int(input("Count of columns: "))
+        print("Example: Id INT AUTO_INCREMENT")
+        for i in range(1, count_of_columns+1):
+            columns = input(f"{i} : ")
+            temp_data_of_columnsArray.append(columns.split()[0])
+            temp_data_of_columns += columns + ", "
+            temp_data_of_columns.strip('\n')
+        #
+        temp_data_of_lines = []
+        count_of_lines = int(input("Count of lines: "))
+        temp_data_of_linesStringStorage = ''
+        for j in range(1, count_of_lines+1):
+            for x in range(1, count_of_columns+1):
+                columns = input(f"line:{j} item:{temp_data_of_columnsArray[x-1]} ") + ","
+
+                #columns = columns.split()
+                #temp_data_of_linesStringStorage += columns + " "
+            #temp_data_of_lines.append(columns)
+            #print(temp_data_of_linesStringStorage)
+        #
+        # cur.execute(f'''CREATE TABLE {DataOfDatabases.table} 
+        # ({temp_data_of_columns})
+        # VALUES ({})''')
 
 #DataOfDatabases = DatabasesNames(input('Database name: ', input("Table name: ")))
 
@@ -148,20 +223,20 @@ def main_menu():
         print("Options:")
         x = ['Create a new table', 'Delete a table', 'Create a new query']
         for i in range(len(x)): print(str(i+1) + ' ' + x[i])
-        match (commands:= int(input())):
-            case 1:
-                command = Commands(1)
-            case 2:
-                command = Commands(2)
-            case 3:
-                command = Selector(3)
-                command.base_method(commands, con, DataOfDatabases.table_name)
-            # case 4:
-            #     command = Commands(4)
-            case unknown_command:
-                print (f"Unknown option '{unknown_command}'")
+        commands = int(input())
+        clear()
+        if commands == 1:
+            command = Creator(1)
+            command.base_method(commands, con, DataOfDatabases)
+        elif commands == 2:
+            command = Commands(2)
+        elif commands == 3:
+            command = Selector(3)
+            command.base_method(commands, con, DataOfDatabases.table_name)
+        else:
+            print (f"Unknown option '{unknown_command}'")
     except psycopg2.OperationalError:
-        print (f'Could not connect to database server')
+        print (f'Could not connect to database server. Check your credentials')
         exit(0)
     finally:
         print("Closed successfully")
