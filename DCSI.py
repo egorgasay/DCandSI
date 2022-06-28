2b625f2f532064dede17eacf1d357289252bb5c5
egorgasay <egorpoletaykin@gmail.com>
2022-06-28 21:47:17 +0300

    Добавлена возможность использования SERIAL PRIMARY KEY для Postgresql, исправлена ошибка возникавшая при использовании Null, исправлена ошибка возникавшая при ручном удалении через запрос.

--- DCSI.py
+++ DCSI.py
@@ -27,7 +27,7 @@ class Query:
                    __         ___                                               2 Simple query
             ()    (_   _. |    |  ._  _|_  _  ._ ._  ._  _  _|_  _  ._
             (_X   __) (_| |   _|_ | |  |_ (/_ |  |_) |  (/_  |_ (/_ |           3 Run from file
-                        |                        |                              
+                        |                        |
             ''')
             decision = input("# ")
             if decision == str(2):
@@ -97,30 +97,49 @@ class Creator:
             temp_data_of_lines = []
             count_of_lines = int(input("Count of lines: "))
             temp_data_of_linesStringStorage = ''
+            temp_data_of_columnsArrayFull = temp_data_of_columns.split(', ')
             try:
+                ignore_list = []
                 for j in range(1, count_of_lines + 1):
                     for x in range(1, count_of_columns + 1):
-                        item = input(
-                            f"line:{j} item:{temp_data_of_columnsArray[x-1]} >")
-                        # todo: сделать обработку неправильного ввода, например когда забыли ввести INT
-                        item = item.split()
-                        item_type = type_finder(item[0])
+                        if 'serial primary key' not in temp_data_of_columnsArrayFull[x - 1].lower():
+                            # todo: сделать обработку неправильного ввода, например когда забыли ввести INT
+                            item = input(
+                                f"line:{j} item:{temp_data_of_columnsArray[x-1]} > ")
+                            item = item.split()
+                            item_type = type_finder(item[0])
 
-                        def if_item_is_null(item, temp_data_of_columnsArray):
-                            if 'null' in item or 'Null' in item or 'NULL' in item:
-                                print(
-                                    f"item:{temp_data_of_columnsArray[x-1]} can't be null")
-                                item = input(
-                                    f"line:{j} item:{temp_data_of_columnsArray[x-1]} >")
-                                if_item_is_null(
-                                    item, temp_data_of_columnsArray[x - 1])
-                            return item
-                        item = if_item_is_null(item, temp_data_of_columnsArray)
-                        if item_type == 'int' or item_type == 'float':
-                            temp_data_of_linesStringStorage += item[0] + ', '
+                            def if_item_is_null(item, temp_data_of_columnsArray, temp_data_of_columnsArrayFull):
+                                if 'null' in item or 'Null' in item or 'NULL' in item:
+                                    # print(temp_data_of_columnsArrayFull[x - 1])
+                                    if 'not null' not in temp_data_of_columnsArrayFull[x - 1].lower():
+                                        # print("Test1")
+                                        item = None
+                                        return item
+                                    print(
+                                        f"item:{temp_data_of_columnsArray[x-1]} can't be null")
+                                    item = input(
+                                        f"line:{j} item:{temp_data_of_columnsArray[x-1]} > ")
+                                    if 'null' in item.lower():
+                                        return if_item_is_null(item, temp_data_of_columnsArray,
+                                                               temp_data_of_columnsArrayFull)
+                                    else:
+                                        return item
+                                else:
+                                    return item
+                            item = if_item_is_null(
+                                item, temp_data_of_columnsArray, temp_data_of_columnsArrayFull)
+                            if item_type == 'int' or item_type == 'float':
+                                temp_data_of_linesStringStorage += item[0] + ', '
+                            else:
+                                if item is None:
+                                    temp_data_of_linesStringStorage += 'NULL' + ', '
+                                else:
+                                    temp_data_of_linesStringStorage += '\'' + \
+                                        item[0] + '\'' + ', '
                         else:
-                            temp_data_of_linesStringStorage += '\'' + \
-                                item[0] + '\'' + ', '
+                            ignore_list.append(
+                                temp_data_of_columnsArrayFull[x - 1])
 
                     temp_data_of_lines.append(temp_data_of_linesStringStorage)
 
@@ -130,20 +149,26 @@ class Creator:
                 input("Back to main menu")
                 main_menu()
             try:
+                ignore_list = list(set(ignore_list))
                 temp_data_of_columns_ready = ''
                 temp_data_of_linesStringStorage2 = ''
-                for i in range(len(temp_data_of_columnsArray)):
-                    temp_data_of_columns_ready += temp_data_of_columnsArray[i] + ", "
-
+                for counter in range(len(ignore_list)):
+                    ignore_list = (" ").join(ignore_list)
+                    print(temp_data_of_columnsArray[counter], ignore_list)
+                    if temp_data_of_columnsArray[counter] in ignore_list:
+                        temp_data_of_columnsArray.pop(counter)
+                temp_data_of_columns_ready = ", ".join(
+                    temp_data_of_columnsArray)
                 for j in range(count_of_lines):
                     temp_data_of_linesStringStorage2 = temp_data_of_lines[j]
                     try:
-                        cur.execute(f'''INSERT INTO {table_name} ({temp_data_of_columns_ready[:-2]})
+                        cur.execute(f'''INSERT INTO {table_name} ({temp_data_of_columns_ready})
                         VALUES ({temp_data_of_linesStringStorage2[:-2]})''')
                     except Exception as e:
                         cur.execute("rollback")
                         print(e)
                         input('Something went wrong while inserting lines..')
+                        clear()
                         main_menu()
                 con.commit()
                 print("Success!")
@@ -153,7 +178,7 @@ class Creator:
                     cur.execute("rollback")
                 query_output_logic(cur.fetchall(), cur.description)
                 input("Back to main menu")
-                clear_screen()
+                clear()
                 main_menu()
             except Exception as e:
                 print(e)
@@ -163,21 +188,37 @@ class Creator:
         def create_only(table_name):
             temp_data_of_columns = ''
             temp_data_of_columnsArray = []
+            temp_data_of_columnsArrayFull = []
             count_of_columns = int(input("Count of columns: "))
-            print("Example: Id INT AUTO_INCREMENT")
+            print("Example: ID SERIAL PRIMARY KEY")
             for i in range(1, count_of_columns + 1):
                 columns = input(f"column:{i} : ")
                 temp_data_of_columnsArray.append(columns.split()[0])
+                temp_data_of_columnsArrayFull.append(columns.split())
                 temp_data_of_columns += columns + ", "
                 temp_data_of_columns = temp_data_of_columns.strip('\n')
             try:
+                # FOR MYSQL in the future
+                # if 'auto_increment' not in temp_data_of_columns.lower():
                 cur.execute(f'''CREATE TABLE {table_name}
                 ({temp_data_of_columns[:-2]})''')
+                # else:
+                #    cur.execute(f'''CREATE TABLE {table_name}
+                #    ({temp_data_of_columns[:-2]+"PRIMARY KEY ({})"})''')
                 con.commit()
+                # if 'SERIAL PRIMARY KEY' in temp_data_of_columnsArrayFull:
+                #     for counter in range(temp_data_of_columnsArrayFull.count("SERIAL PRIMARY KEY")):
+                #         temp_data_of_columnsArrayFull.index(value)
                 print("Success!")
+                # if temp_data_of_columns
+                input("Next")
+                clear()
             except Exception as e:
-                print("Error!", e)
                 cur.execute("rollback")
+                print(e)
+                input('Something went wrong while creating table..')
+                clear()
+                main_menu()
 
             return temp_data_of_columns, temp_data_of_columnsArray, count_of_columns
 
@@ -249,7 +290,19 @@ def create_and_execute_ready_query(user_query):
         cur.execute(f'''{query}''')
     except:
         cur.execute("rollback")
-    query_output_logic(cur.fetchall(), cur.description)
+        input("Back to main menu")
+        clear()
+        main_menu()
+    con.commit()
+    try:
+        query_output_logic(cur.fetchall(), cur.description)
+    except Exception as e:
+        if 'DROP TABLE' in query.upper() and 'no results to fetch' in str(e):
+            print("Deleted successfully")
+        else:
+            print(f"Error in query text!{e}!")
+            input("Back to main menu")
+            main_menu()
     save_decision = input("Save the query text? (Y/n): ")
     if save_decision.lower() == 'n' or save_decision.lower() == 'no':
         pass
